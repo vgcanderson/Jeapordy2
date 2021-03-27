2e462214472cdf7fedc9a0ca32c171eb804b0f36
Chris Anderson <chrisanderson@velocityglobal.com>
2021-03-27 19:03:11 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// This is the JavaScript entry file - your code begins here
+// Do not delete or rename this file ********
+
+// An example of how you tell webpack to use a CSS (SCSS) file
+import './css/base.css';
+
+// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import '../public/images/turing-logo.png'
+
+// console.log('This is the JavaScript entry file - your code begins here.');
+
+// document.getElementById("h1").innerHTML = "Hello World 2";
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const webpack = require("webpack");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-eval-source-map",
+  entry: {
+    main: path.resolve(process.cwd(), "src", "main.js")
+  },
+  output: {
+    path: path.resolve(process.cwd(), "dist"),
+    publicPath: "/"
+  },
+  watchOptions: {
+    // ignored: /node_modules/,
+    aggregateTimeout: 300, // After seeing an edit, wait .3 seconds to recompile
+    poll: 500 // Check for edits every 5 seconds
+  },
+    module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'images/',
+                  publicPath: 'images/'
+                }
+              }
+            ]
+      }
+    ],
+  },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin(),
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(process.cwd(), "public", "index.html")
+    })
+  ]
+}
